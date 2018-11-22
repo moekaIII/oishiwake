5595e190bf3924b06cf21dea278756e3a3db5b5b
ihara moeka <ihara.moeka@gmail.com>
2018-11-22 11:18:33 +0900

    シードデータの挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+30.times do |n|
+  name = Faker::Pokemon.name
+  email = Faker::Internet.email
+  avatar = Faker::Avatar.image("my-own-slug")
+  place = Faker::Address.state
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               icon: avatar,
+               place: place
+               )
+end
