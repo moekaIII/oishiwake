9c6554b5f26eeceb546b215849a4337422b54d87
ihara moeka <ihara.moeka@gmail.com>
2018-11-27 18:14:25 +0900

    投稿に過去の日にちを設定できないバリデーションを追加

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,14 @@
 class Dish < ApplicationRecord
   validates :name, length: {in: 1..255}
   validates :tip, presence: true
+  validate :date_cannot_be_in_the_past
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+
+  def date_cannot_be_in_the_past
+    if date < Date.today
+      errors.add(:date, "は未来の日にちを入力してください")
+    end
+  end
 end
